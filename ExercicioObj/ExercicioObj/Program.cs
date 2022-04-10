905ba50e29e8eebe4865d8628b417655211dba8f
danny-for <dannygeovanna2019@gmail.com>
2022-04-10 19:36:24 -0300

    @Danny-for [Objetos]

---
+++ ExercicioObj/ExercicioObj/Program.cs
@@ -0,0 +1,23 @@
+﻿using ExercicioObj.src;
+using System;
+
+namespace ExercicioObj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Atividade classe Desenho da  Catarina");
+
+            //instanciando
+            Coelho sasao = new Coelho ("sasao" , "azul" , "acessorio");
+
+            
+            sasao.Pulando();
+            sasao.Comer();
+
+            Console.WriteLine($"Catarina fez uma {sasao.Name} e sua cor {sasao.Cor}");
+
+        }
+    }
+}
---
+++ ExercicioObj/ExercicioObj/src/Coelho.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioObj.src
+{
+    class Coelho
+    {
+
+        //atributos
+        public string Name { get; set; }//pega e altera
+        public string Cor { get; set; }
+
+
+        // construtor do coelho 
+        public Coelho(string _name , string _cor , string _acessorio )
+        {
+            Name = _name;
+            Cor = _cor;
+            acessorio = _acessorio;
+        }
+
+        //metodos //ações 
+
+        public void Comer()
+        {
+            Console.WriteLine("Coelho comendo cenoura ;)");
+        }
+
+        public void Pulando()// printa sem retorno
+        {
+            Console.WriteLine("Coelho está pulando no campo");
+        }
+
+        public string Brincando()// não printa mais dá o retorno 
+        {
+            return ("Coelho brincando");
+        }
+
+
+        
+    }
+
+
+}
